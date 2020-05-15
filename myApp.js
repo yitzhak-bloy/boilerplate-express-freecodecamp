6ef23929d5ea1578f11f554b8cf4996a7755660d
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-05-15 15:55:55 +0300

    Use the .env File

--- myApp.js
+++ myApp.js
@@ -11,16 +11,26 @@ var app = express();
 console.log("Hello World")
 
 /** 2) A first working Express Server */
-
+// app.get('/', ((req, res) => {
+//   res.send('Hello Express');
+// }))
 
 /** 3) Serve an HTML file */
-
+app.get('/', ((req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+}))
 
 /** 4) Serve static assets  */
-
+app.use(express.static(__dirname + '/public'))
 
 /** 5) serve JSON on a specific route */
-
+app.get("/json", ((req,res) => {
+  var msgobj = {"message" :"Hello json"};
+  if(process.env.MESSAGE_STYLE == 'uppercase'){
+     msgobj.message = msgobj.message.toUpperCase();
+    }
+  res.json(msgobj);
+}));
 
 /** 6) Use the .env file to configure the app */
  
@@ -49,7 +59,7 @@ console.log("Hello World")
 
 // This would be part of the basic setup of an Express app
 // but to allow FCC to run tests, the server is already active
-/** app.listen(process.env.PORT || 3000 ); */
+ app.listen(process.env.PORT || 3000 ); 
 
 //---------- DO NOT EDIT BELOW THIS LINE --------------------
 
--- server.js
+++ server.js
@@ -4,6 +4,7 @@
  * ***************************************************/
  
 var bGround = require('fcc-express-bground');
+require('dotenv').config()
 var myApp = require('./myApp');
 var express = require('express');
 var app = express();
